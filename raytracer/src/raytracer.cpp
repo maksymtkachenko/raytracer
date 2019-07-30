229c398f57e73f60f0c7e1e74aa689a086cabfee
Maksym Tkachenko <maxtkachenko.vfx@gmail.com>
2019-07-30 18:05:34 -0500

    Write ray and vec3 class

--- raytracer/src/raytracer.cpp
+++ raytracer/src/raytracer.cpp
@@ -1,15 +1,40 @@
-//============================================================================
-// Name        : raytracer.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include "ray.h"
+
+vec3 color(const ray& r) {
+	vec3 unit_direction = unit_vector(r.direction());
+
+	return unit_direction;
+}
+
 
 int main() {
-	cout << "Hello This is my first project created from Eclipse on mac !!!" << endl; // prints !!!Hello World!!!
+	int nx = 400;
+	int ny = 200;
+
+	std::ofstream imageFile ("image.ppm");
+	if (imageFile.is_open())
+	{
+	  imageFile << "P3\n" << nx << " " << ny << "\n255\n";
+
+	  vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	  vec3 horizontal(4.0, 0.0, 0.0);
+	  vec3 vertical(0.0, 2.0, 0.0);
+	  vec3 origin(0.0, 0.0, 0.0);
+
+	  for(int j = ny-1; j >= 0; j--) {
+		  for(int i = 0; i < nx; i++) {
+			  float u = float(i) / float(nx);
+			  float v = float(j) / float(ny);
+			  ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+			  vec3 col = color(r);
+		  }
+	  }
+
+	}
+
+
+
 	return 0;
 }
